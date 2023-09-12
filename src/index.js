f4cd520be46d7f63de889ccff49998ef80e906bc
jeffmanhart <jeffreymanhart@gmail.com>
2023-09-12 14:19:37 -0600

    taskbuilder created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      buildToDo(e.target["new-task-description"].value)
+    })
 });
+
+
+function buildToDo(task) {
+  let li = document.createElement('li')
+  li.textContent = task
+  document.querySelector('#tasks').appendChild(li)
+}
